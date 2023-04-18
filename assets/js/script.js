1ec7269675480530d753ba05dd3c843841665813
Oscar Lundsör <oscarlundsor@gmail.com>
2023-04-18 15:58:07 +0000

    testing ground for my button functions

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+function removeElement() {
+    document.getElementById("imgbox1").style.display = "block";
+}
+
+function removeElement2() {
+    document.getElementById("imgbox2").style.display = "block";
+}
+
+function removeElement3() {
+    document.getElementById("imgbox3").style.display = "block";
+}
+
+function removeElement4() {
+    document.getElementById("imgbox4").style.display = "block";
+}
+
+function resetElement() {
+    document.getElementById("imgbox1").style.display = "none";
+    document.getElementById("imgbox2").style.display = "none";
+    document.getElementById("imgbox4").style.display = "none";
+    document.getElementById("imgbox3").style.display = "none";
+}
